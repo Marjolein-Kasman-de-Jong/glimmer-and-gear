49fbd0777ac89efc5e8bb38f27641b088d83fddc
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-03-28 10:37:21 +0100

    started working on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,32 @@
+// Images
+import magnifyingGlass from '../../assets/magnifying-glass-solid.svg';
+import shoppingCart from '../../assets/icon-shopping-cart.jpeg';
+
 // Style
-import './navbar.css'
+import './navbar.css';
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            navbar
+            <nav>
+                <ul>
+                    <li>Categories</li>
+                    <li>FAQs</li>
+                    <li>Contact</li>
+                </ul>
+                <div className='search-field-wrapper'>
+                    <input type='text' />
+                    <img src={magnifyingGlass} alt='Magnifying glass icon' className='magnifying-glass'/>
+                </div>
+                <ul>
+                    <li>Login</li>
+                    <li>
+                        <img src={shoppingCart} alt='Shopping cart icon' className='shopping-cart'/>
+                    </li>
+                </ul>
+            </nav>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
