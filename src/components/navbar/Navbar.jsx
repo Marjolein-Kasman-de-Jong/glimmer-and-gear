aa67384e2888eb1648ef79208db52139bac17dd0
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-03-29 16:22:23 +0100

    started over, finished title bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,9 @@
-// Images
-import magnifyingGlass from '../../assets/icon-magnifying-glass.svg';
-import shoppingCart from '../../assets/icon-shopping-cart.jpeg';
-
 // Style
 import './navbar.css';
 
 const Navbar = () => {
     return (
-        <div className='navbar'>
-            <nav>
-                {/* Menu 1 */}
-                <ul>
-                    <li>Categories</li>
-                    <li>FAQs</li>
-                    <li>Contact</li>
-                </ul>
-                {/* Search bar */}
-                <div className='search-field-wrapper'>
-                    <input type='text' placeholder='What are you looking for?' />
-                    <img src={magnifyingGlass} alt='Magnifying glass icon' className='magnifying-glass'/>
-                </div>
-                {/* Menu 2 */}
-                <ul>
-                    <li>Login</li>
-                    <li>
-                        <img src={shoppingCart} alt='Shopping cart icon' className='shopping-cart'/>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <></>
     );
 }
 
--- src/components/titlebar/Titlebar.jsx
+++ src/components/titlebar/Titlebar.jsx
@@ -7,14 +7,12 @@ import './titlebar.css';
 const Titlebar = () => {
     return (
         <div className='titlebar'>
-            {/* Logo */}
             <div className='logo-wrapper'>
-                <img src={logo} alt='Glimmer & Gear logo' />
+                <img src={logo} alt='Glimmer & Gear Logo' />
             </div>
-            {/* Shop name and tagline */}
             <hgroup>
-                <h1>Glimmer & Gear</h1>
-                <p>Shine with style, thrive with tech</p>
+                <h1>glimmer & gear</h1>
+                <p>shine with style, thrive with tech</p>
             </hgroup>
         </div>
     );
