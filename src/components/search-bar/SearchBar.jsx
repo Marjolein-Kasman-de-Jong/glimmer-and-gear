c974abd56dc26af9db97c754fd5753e2a81c219a
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-03 13:19:33 +0200

    created search bar

---
+++ src/components/search-bar/SearchBar.jsx
@@ -0,0 +1,18 @@
+// Icons
+import { SlMagnifier } from "react-icons/sl";
+
+// Style
+import './search-bar.css';
+
+const SearchBar = () => {
+    return (
+        <aside className='search-bar'>
+            <form action='#'>
+                <input type="text" name='search-field' id='search-field' placeholder='What are you looking for?' />
+                <SlMagnifier class='search-icon' />
+            </form>
+        </aside>
+    );
+}
+
+export default SearchBar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
+// Components
+import SearchBar from '../../components/search-bar/SearchBar'
+
 // Style
 import './home.css'
 
 const Home = () => {
     return (
         <main>
+            <SearchBar />
             <h2>Home</h2>
         </main>
     )
