9dcc4e8763bdcbf1c0d8ed265c105cf17e028e08
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-03 10:27:32 +0200

    created getCurrentYear helper and socials

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,38 @@
+// Helpers
+import getCurrentYear from '../../helpers/getCurrentYear';
+
 // Images
+import facebook from '../../assets/icon-facebook.svg';
+import instagram from '../../assets/icon-instagram.svg';
+import twitter from '../../assets/icon-twitter.svg';
 
 // Style
 import './footer.css';
 
 const Footer = () => {
+    const currentYear = getCurrentYear();
+
     return (
         <footer>
-            Footer
-        </footer>
+            <p>{`glimmer and gear ${currentYear}`}</p>
+            <div className='socials'>
+                <a href='https://www.facebook.com/' target='_blank'>
+                    <div className='icon-container'>
+                        <img src={facebook} alt='Facebook icon' />
+                    </div>
+                </a>
+                <a href='https://www.instagram.com/' target='_blank'>
+                    <div className='icon-container'>
+                        <img src={instagram} alt='Instagram icon' />
+                    </div>
+                </a>
+                <a href='https://twitter.com/' target='_blank'>
+                    <div className='icon-container'>
+                        <img src={twitter} alt='X/Twitter icon' />
+                    </div>
+                </a>
+            </div >
+        </footer >
     );
 }
 
