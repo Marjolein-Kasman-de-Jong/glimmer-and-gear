21cf6676021e4c1e8dba851cf9264621698a38c3
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-08 15:46:06 +0200

    created not found page

--- src/pages/page-not-found/PageNotFound.jsx
+++ src/pages/page-not-found/PageNotFound.jsx
@@ -1,10 +1,22 @@
+// Images
+import notFoundImage from '../../assets/page-not-found.jpeg';
+
 // Style
 import './page-not-found.css'
 
 const PageNotFound = () => {
     return (
-        <main>
-            <h2>Page not found</h2>
+        <main className='not-found-container'>
+            <article className='not-found-text-container'>
+                <header>
+                    <h2>So sorry!</h2>
+                </header>
+                <p>The page you are looking for cannot be found.</p>
+                <button type="button">Take me home</button>
+            </article>
+            <div className="not-found-image-container">
+                <img src={notFoundImage} alt='Man that is lost' />
+            </div>
         </main>
     )
 }
