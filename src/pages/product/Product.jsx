74d5c1c0da258c27c0eb497c3b308fae6a22746a
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-09 08:44:23 +0200

    routecreated dynamic link

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 // Components
 import SearchBar from '../../components/search-bar/SearchBar';
@@ -9,11 +11,29 @@ import './product.css';
 const Product = () => {
     const { id } = useParams();
 
+    const [productData, setProductData] = useState({});
+
+    useEffect(() => {
+        async function getProductData() {
+            try {
+                const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+                setProductData(response);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getProductData();
+    }, [])
+
+    console.log(productData)
+    
     return (
         <main>
             <SearchBar />
-            <h2>Product page</h2>
-            <p>{id}</p>
+            <h2>{productData?.data?.title}</h2>
+            <article>
+
+            </article>
         </main>
     )
 }
