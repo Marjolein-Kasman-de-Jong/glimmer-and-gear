f2826c3ee1294edc5c6c3b89a22c172d56b90ba8
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-09 10:19:00 +0200

    added some content and styling

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,19 +1,51 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 // Components
 import SearchBar from '../../components/search-bar/SearchBar';
+
+// Icons
+import { TiStar } from 'react-icons/ti';
 
 // Style
 import './product.css';
 
 const Product = () => {
+    // Get product id
     const { id } = useParams();
+
+    // Get product data
+    const [productData, setProductData] = useState({});
+    console.log(productData)
+
+    useEffect(() => {
+        async function getProductData() {
+            try {
+                const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+                setProductData(response);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getProductData();
+    }, [])
 
     return (
         <main>
             <SearchBar />
-            <h2>Product page</h2>
-            <p>{id}</p>
+            <h2>{productData?.data?.title}</h2>
+            <article className='single-product'>
+                <div className='single-product-image-container'>
+                    <img src={productData?.data?.image} alt={productData?.data?.title} />
+                    <p>{productData?.data?.price}</p>
+                    <TiStar className='rating-star' />
+                    <p className='rating'>{productData?.data?.rating?.rate}</p>
+                </div>
+                <div className="single-product-text-container">
+                    <p>{productData?.data?.description}</p>
+                </div>
+            </article>
         </main>
     )
 }
