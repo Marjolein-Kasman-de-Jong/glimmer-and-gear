304d0e22626a6d987ab4c017a5d9545b52e1dbd2
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-09 08:56:34 +0200

    created dynamic route

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,4 +1,4 @@
-
+import { NavLink } from 'react-router-dom';
 
 // Icons
 import { TiStar } from 'react-icons/ti';
@@ -7,18 +7,22 @@ import { TiStar } from 'react-icons/ti';
 import './product-card.css';
 
 const ProductCard = ({ product }) => {
+    const {id, rating, title, price} = product;
+
     return (
-        <article className='product-card'>
-            <div className='image-container'>
-                <TiStar className='rating-star' />
-                <p className='product-rating'>{product?.rating.rate}</p>
-                <img src={product?.image} alt={product?.title} />
-            </div>
-            <div className='text-container'>
-                <h3>{product?.title}</h3>
-                <p>{product?.price}</p>
-            </div>
-        </article>
+        <NavLink to={`/product/${id}`}>
+            <article className='product-card'>
+                <div className='image-container'>
+                    <TiStar className='rating-star' />
+                    <p className='product-rating'>{rating.rate}</p>
+                    <img src={product?.image} alt={title} />
+                </div>
+                <div className='text-container'>
+                    <h3>{title}</h3>
+                    <p>{price}</p>
+                </div>
+            </article>
+        </NavLink>
     );
 }
 
