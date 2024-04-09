bc2639f7db78585f04b626eccd44e3ace96ff641
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-09 08:46:08 +0200

    Revert "routecreated dynamic link"
    This reverts commit 74d5c1c0da258c27c0eb497c3b308fae6a22746a.

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+
 
 // Icons
 import { TiStar } from 'react-icons/ti';
@@ -8,19 +8,17 @@ import './product-card.css';
 
 const ProductCard = ({ product }) => {
     return (
-        <NavLink to={`/product/${product.id}`}>
-            <article className='product-card'>
-                <div className='image-container'>
-                    <TiStar className='rating-star' />
-                    <p className='product-rating'>{product?.rating.rate}</p>
-                    <img src={product?.image} alt={product?.title} />
-                </div>
-                <div className='text-container'>
-                    <h3>{product?.title}</h3>
-                    <p>{product?.price}</p>
-                </div>
-            </article>
-        </NavLink>
+        <article className='product-card'>
+            <div className='image-container'>
+                <TiStar className='rating-star' />
+                <p className='product-rating'>{product?.rating.rate}</p>
+                <img src={product?.image} alt={product?.title} />
+            </div>
+            <div className='text-container'>
+                <h3>{product?.title}</h3>
+                <p>{product?.price}</p>
+            </div>
+        </article>
     );
 }
 
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
 
 // Components
 import SearchBar from '../../components/search-bar/SearchBar';
@@ -11,29 +9,11 @@ import './product.css';
 const Product = () => {
     const { id } = useParams();
 
-    const [productData, setProductData] = useState({});
-
-    useEffect(() => {
-        async function getProductData() {
-            try {
-                const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
-                setProductData(response);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        getProductData();
-    }, [])
-
-    console.log(productData)
-    
     return (
         <main>
             <SearchBar />
-            <h2>{productData?.data?.title}</h2>
-            <article>
-
-            </article>
+            <h2>Product page</h2>
+            <p>{id}</p>
         </main>
     )
 }
