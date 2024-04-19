62c4434c86c89641d00151895041ea1309f4f23c
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-19 20:29:50 +0200

    created contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,12 @@
 // Components
-import SearchBar from '../../components/search-bar/SearchBar'
+import SearchBar from '../../components/search-bar/SearchBar';
 
 // Style
-import './contact.css'
+import './contact.css';
+
+// Images
+import socials from '../../assets/contact-us-socials.jpeg';
+import email from '../../assets/contact-us-email.jpeg';
 
 const Contact = () => {
     return (
@@ -14,15 +18,21 @@ const Contact = () => {
             <div className='contact-information-container'>
                 <div className='socials-container'>
                     <div className='socials'>socials</div>
-                    <div className='contact-image-wrapper'>image</div>
+                    <div className='contact-image-wrapper'>
+                        <img src={socials} alt='Social media logos' />
+                    </div>
                 </div>
                 <div className='email-container'>
-                    <div className='contact-image-wrapper'>image</div>
-                    <div className='email'>email</div>
+                    <div className='contact-image-wrapper'>
+                        <img src={email} alt='Woman working on a laptop computer' />
+                    </div>
+                    <div className='email'>
+                        <p>customerservice@glimmerandgear.com</p>
+                    </div>
                 </div>
             </div>
         </main>
     )
 }
 
-export default Contact
+export default Contact;
