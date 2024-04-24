b2fc1cad0cb4885e8edfc6245c203b587981d985
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-24 16:01:54 +0200

    made code more readable

--- src/components/profile-item/ProfileItem.jsx
+++ src/components/profile-item/ProfileItem.jsx
@@ -7,7 +7,12 @@ const ProfileItem = ({ item }) => {
 
     return (
         <div className='profile-item-container'>
-            <h4>{itemName}</h4><p>{itemValue}</p>
+            <h4>
+                {itemName}
+            </h4>
+            <p>
+                {itemValue}
+            </p>
         </div>
     );
 }
