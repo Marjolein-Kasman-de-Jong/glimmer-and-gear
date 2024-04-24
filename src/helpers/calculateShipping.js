0bd4f2bce2b279ea03e8a52136cdf2f32817204b
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-24 12:33:49 +0200

    added social links

--- src/helpers/calculateShipping.js
+++
@@ -1,6 +0,0 @@
-function calculateShipping(productsTotal) {
-    const shipping = productsTotal > 20 ? 0 : 9.99;
-    return shipping;
-}
-
-export default calculateShipping;
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,12 +1,18 @@
 // Components
 import SearchBar from '../../components/search-bar/SearchBar';
-
-// Style
-import './contact.css';
+import SocialLink from '../../components/social-link/SocialLink';
 
 // Images
 import socials from '../../assets/contact-us-socials.jpeg';
 import email from '../../assets/contact-us-email.jpeg';
+
+// Icons
+import { TiSocialFacebook } from "react-icons/ti";
+import { TiSocialInstagram } from "react-icons/ti";
+import { TiSocialTwitter } from "react-icons/ti";
+
+// Style
+import './contact.css';
 
 const Contact = () => {
     return (
@@ -17,7 +23,26 @@ const Contact = () => {
             </header>
             <div className='contact-information-container'>
                 <div className='socials-container'>
-                    <div className='socials'>socials</div>
+                    <div className='socials'>
+                        <SocialLink href='https://www.facebook.com/' alt='Facebook icon'>
+                            <div className='social-item-wrapper'> {/* Needed to allow both children to be rendered, not just one */}
+                                <TiSocialFacebook className='social-link-icon' />
+                                <p>Facebook</p>
+                            </div>
+                        </SocialLink>
+                        <SocialLink href='https://www.instagram.com/' alt='Instagram icon'>
+                            <div className='social-item-wrapper'> {/* Needed to allow both children to be rendered, not just one */}
+                                <TiSocialInstagram className='social-link-icon' />
+                                <p>Instagram</p>
+                            </div>
+                        </SocialLink>
+                        <SocialLink href='https://twitter.com/' alt='X/Twitter icon'>
+                            <div className='social-item-wrapper'> {/* Needed to allow both children to be rendered, not just one */}
+                                <TiSocialTwitter className='social-link-icon' />
+                                <p>Twitter</p>
+                            </div>
+                        </SocialLink>
+                    </div>
                     <div className='contact-image-wrapper'>
                         <img src={socials} alt='Social media logos' />
                     </div>
