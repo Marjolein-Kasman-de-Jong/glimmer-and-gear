1314e2b87beb36b1a8a70444c8fa4a408d44b81f
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-24 13:34:03 +0200

    made category tiles navlinks

--- src/components/category-tile/CategoryTile.jsx
+++ src/components/category-tile/CategoryTile.jsx
@@ -1,14 +1,18 @@
+import { NavLink } from 'react-router-dom'
+
 // Style
 import './category-tile.css';
 
 const CategoryTile = ({ category }) => {
     return (
-        <figure className='category-tile'>
-            <img src={`../../src/assets/${category.image}`} alt={category.title} />
-            <figcaption className='category-tile-title'>
-                <h3>{category.title}</h3>
-            </figcaption>
-        </figure>
+        <NavLink to={`/category/${category.category}`}>
+            <figure className='category-tile'>
+                <img src={`../../src/assets/${category.image}`} alt={category.title} />
+                <figcaption className='category-tile-title'>
+                    <h3>{category.title}</h3>
+                </figcaption>
+            </figure>
+        </NavLink>
     );
 }
 
