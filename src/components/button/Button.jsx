a1324794c52222aa2d41ecd719fd6a564b0b7838
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-24 15:04:51 +0200

    made code more readable

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,9 +2,12 @@
 import './button.css';
 
 const Button = ({ type, buttonText, icon, onClick, id }) => {
-
     return (
-        <button type={type} className='regular-button' onClick={id ? () => onClick(id) : onClick}>
+        <button
+            type={type}
+            className='regular-button'
+            onClick={id ? () => onClick(id) : onClick}
+        >
             {buttonText}
             {icon}
         </button>
