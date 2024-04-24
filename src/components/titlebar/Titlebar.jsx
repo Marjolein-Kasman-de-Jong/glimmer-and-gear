7972cc40699bc7e2a61c9fff259ef8d4b82785fb
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-24 13:02:31 +0200

    made logo and shop name navlinks

--- src/components/titlebar/Titlebar.jsx
+++ src/components/titlebar/Titlebar.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom';
+
 // Images
 import logo from '../../assets/logo.jpeg';
 
@@ -8,12 +10,16 @@ const Titlebar = () => {
     return (
         <div className='titlebar'>
             {/* Logo */}
-            <img src={logo} alt='Glimmer & Gear logo' />
+            <NavLink to='/'>
+                <img src={logo} alt='Glimmer & Gear logo' />
+            </NavLink>
             {/* Shop name and tagline */}
-            <hgroup>
-                <h1>glimmer & gear</h1>
-                <p className='titlebar-p'>shine with style, thrive with tech</p>
-            </hgroup>
+            <NavLink to='/'>
+                <hgroup>
+                    <h1>glimmer & gear</h1>
+                    <p className='titlebar-p'>shine with style, thrive with tech</p>
+                </hgroup>
+            </NavLink>
         </div>
     );
 }
