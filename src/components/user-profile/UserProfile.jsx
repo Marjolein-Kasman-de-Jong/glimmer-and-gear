e8009b6f75fbd5752cf6b921da9d1e9a3862f1d7
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-24 16:37:02 +0200

    made code more readable

--- src/components/user-profile/UserProfile.jsx
+++ src/components/user-profile/UserProfile.jsx
@@ -3,24 +3,35 @@ import ProfileItem from '../profile-item/ProfileItem';
 import Button from '../button/Button';
 
 // Icons
-import { SlPencil } from "react-icons/sl";
-
-// Style
-import './user-profile.css';
+import { SlPencil } from 'react-icons/sl';
 
 const UserProfile = ({ statusCode, statusMessage, username, email, info, edit, toggleEdit }) => {
     const icon = <SlPencil />;
-    
+
     return (
         <>
-            {statusMessage && <p className={`statusCode-${statusCode}`}>{statusMessage}</p>}
+            {/* Status message */}
+            {
+                statusMessage &&
+                <p className={`statusCode-${statusCode}`}>
+                    {statusMessage}
+                </p>
+            }
+            {/* User profile */}
             <article className='profile-container'>
                 <header className='profile-title'>
-                    <h3>{username}</h3>
-                    <Button type='button' buttonText='edit' icon={icon} onClick={() => toggleEdit(!edit)} />
+                    <h3>
+                        {username}
+                    </h3>
+                    <Button
+                        type='button'
+                        buttonText='edit'
+                        icon={icon}
+                        onClick={() => toggleEdit(!edit)}
+                    />
                 </header>
-                <ProfileItem item={{ email: email }} />
-                <ProfileItem item={{ info: info }} />
+                <ProfileItem item={{ email }} />
+                <ProfileItem item={{ info }} />
             </article>
         </>
     );
