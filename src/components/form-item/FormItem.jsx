edc0c099e58cd26f7982b4233b4af568e15441ea
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-24 15:37:32 +0200

    made code more readable

--- src/components/form-item/FormItem.jsx
+++ src/components/form-item/FormItem.jsx
@@ -7,15 +7,35 @@ const FormItem = ({ item, type, formState, handleChange, errorMessages }) => {
     return (
         <div className='form-item-wrapper'>
             <div className='form-item-container'>
-                <label htmlFor={item}>{item}</label>
+                <label htmlFor={item}>
+                    {item}
+                </label>
                 {
                     item === 'info' ?
-                        <textarea type={type} name={item} id={item} value={formState[item]} onChange={handleChange}></textarea>
+                        <textarea
+                            type={type}
+                            name={item}
+                            id={item}
+                            value={formState[item]}
+                            onChange={handleChange}
+                        >
+                        </textarea>
                         :
-                        <input type={type} name={item} id={item} value={formState[item]} onChange={handleChange} />
+                        <input
+                            type={type}
+                            name={item}
+                            id={item}
+                            value={formState[item]}
+                            onChange={handleChange}
+                        />
                 }
             </div>
-            {errorMessages[error] && <p className='error-message'>{errorMessages[error]}</p>}
+            {
+                errorMessages[error] &&
+                <p className='error-message'>
+                    {errorMessages[error]}
+                </p>
+            }
         </div>
     );
 }
