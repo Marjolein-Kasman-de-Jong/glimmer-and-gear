d786ad529e0bc6d8f01786d85879d1e0b8279315
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-25 08:54:36 +0200

    made code more readable

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -7,9 +7,9 @@ import socials from '../../assets/contact-us-socials.jpeg';
 import email from '../../assets/contact-us-email.jpeg';
 
 // Icons
-import { TiSocialFacebook } from "react-icons/ti";
-import { TiSocialInstagram } from "react-icons/ti";
-import { TiSocialTwitter } from "react-icons/ti";
+import { TiSocialFacebook } from 'react-icons/ti';
+import { TiSocialInstagram } from 'react-icons/ti';
+import { TiSocialTwitter } from 'react-icons/ti';
 
 // Style
 import './contact.css';
@@ -19,45 +19,70 @@ const Contact = () => {
         <main>
             <SearchBar />
             <header>
-                <h2>Contact us</h2>
+                <h2>
+                    Contact us
+                </h2>
             </header>
             <div className='contact-information-container'>
                 <div className='socials-container'>
                     <div className='socials'>
-                        <SocialLink href='https://www.facebook.com/' alt='Facebook icon'>
+                        <SocialLink
+                            href='https://www.facebook.com/'
+                            alt='Facebook icon'
+                        >
                             <div className='social-item-wrapper'> {/* Needed to allow both children to be rendered, not just one */}
                                 <TiSocialFacebook className='social-link-icon' />
-                                <p>Facebook</p>
+                                <p>
+                                    Facebook
+                                </p>
                             </div>
                         </SocialLink>
-                        <SocialLink href='https://www.instagram.com/' alt='Instagram icon'>
-                            <div className='social-item-wrapper'> {/* Needed to allow both children to be rendered, not just one */}
+                        <SocialLink
+                            href='https://www.instagram.com/'
+                            alt='Instagram icon'
+                        >
+                            <div className='social-item-wrapper'>
                                 <TiSocialInstagram className='social-link-icon' />
-                                <p>Instagram</p>
+                                <p>
+                                    Instagram
+                                </p>
                             </div>
                         </SocialLink>
-                        <SocialLink href='https://twitter.com/' alt='X/Twitter icon'>
-                            <div className='social-item-wrapper'> {/* Needed to allow both children to be rendered, not just one */}
+                        <SocialLink
+                            href='https://twitter.com/'
+                            alt='X/Twitter icon'
+                        >
+                            <div className='social-item-wrapper'>
                                 <TiSocialTwitter className='social-link-icon' />
-                                <p>Twitter</p>
+                                <p>
+                                    Twitter
+                                </p>
                             </div>
                         </SocialLink>
                     </div>
                     <div className='contact-image-wrapper'>
-                        <img src={socials} alt='Social media logos' />
+                        <img
+                            src={socials}
+                            alt='Social media logos'
+                        />
                     </div>
                 </div>
                 <div className='email-container'>
                     <div className='contact-image-wrapper'>
-                        <img src={email} alt='Woman working on a laptop computer' />
+                        <img
+                            src={email}
+                            alt='Woman working on a laptop computer'
+                        />
                     </div>
                     <div className='email'>
-                        <p>customerservice@<wbr />glimmerandgear<wbr />.com</p>
+                        <p>
+                            customerservice@<wbr />glimmerandgear<wbr />.com
+                        </p>
                     </div>
                 </div>
             </div>
         </main>
-    )
+    );
 }
 
 export default Contact;
