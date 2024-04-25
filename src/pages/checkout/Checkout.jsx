d39ac64daef1f3f991fc8814ece54f38499948e0
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-25 08:50:01 +0200

    made code more readable

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -17,51 +17,83 @@ const Checkout = () => {
     return (
         <main>
             <header>
-                <h2>Checkout page</h2>
+                <h2>
+                    Checkout page
+                </h2>
             </header>
             <div className='checkout-container'>
                 <article className='shipping-address-container'>
                     <header>
-                        <h3>Shipping address</h3>
+                        <h3>
+                            Shipping address
+                        </h3>
                     </header>
                     <div className='shipping-address'>
                         <div className='name'>
-                            <p>Name</p>
-                            <p>{username}</p>
+                            <p>
+                                Name
+                            </p>
+                            <p>
+                                {username}
+                            </p>
                         </div>
                         <div className='address'>
-                            <p>Address</p>
-                            <p>Backend doesn't allow storing this data.</p>
+                            <p>
+                                Address
+                            </p>
+                            <p>
+                                Backend doesn't allow storing this data.
+                            </p>
                         </div>
                     </div>
                 </article>
                 <article className='order-container'>
                     <header>
-                        <h3>Order</h3>
+                        <h3>
+                            Order
+                        </h3>
                     </header>
                     <div className='order'>
                         {
                             shoppingCart.map((item) => {
                                 return (
                                     <div className='ordered-item'>
-                                        <p>{item.itemName}</p>
-                                        <p>{item.amount}</p>
-                                        <p>{(item.price * item.amount).toFixed(2)}</p>
+                                        <p>
+                                            {item.itemName}
+                                        </p>
+                                        <p>
+                                            {item.amount}
+                                        </p>
+                                        <p>
+                                            {(item.price * item.amount).toFixed(2)}
+                                        </p>
                                     </div>
                                 );
                             })
                         }
                         <div className='shipping'>
-                            <p>Shipping</p>
-                            <p>{shipping}</p>
+                            <p>
+                                Shipping
+                            </p>
+                            <p>
+                                {shipping}
+                            </p>
                         </div>
                         <div className='total'>
-                            <p>Total</p>
-                            <p>{grandTotal.toFixed(2)}</p>
+                            <p>
+                                Total
+                            </p>
+                            <p>
+                                {grandTotal.toFixed(2)}
+                            </p>
                         </div>
                     </div>
                 </article>
-                <Button type='button' buttonText='Confirm order and choose payment option' onClick={() => alert('We haven\'t learned how to add payment options yet. ;-))')}/>
+                <Button
+                    type='button'
+                    buttonText='Confirm order and choose payment option'
+                    onClick={() => alert('We haven\'t learned how to add payment options yet. ;-))')}
+                />
             </div>
         </main>
     );
