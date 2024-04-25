82b747fb15c6831f8fa7a3dfeef8bc58abcdf96d
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-04-25 09:16:48 +0200

    feature/pagenotfound

--- src/pages/page-not-found/PageNotFound.jsx
+++ src/pages/page-not-found/PageNotFound.jsx
@@ -1,4 +1,4 @@
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 // Components
 import Button from '../../components/button/Button';
@@ -7,25 +7,36 @@ import Button from '../../components/button/Button';
 import notFoundImage from '../../assets/page-not-found.jpeg';
 
 // Style
-import './page-not-found.css'
+import './page-not-found.css';
 
 const PageNotFound = () => {
-    const navigate = useNavigate()    
+    const navigate = useNavigate();
 
     return (
         <main className='not-found-container'>
             <article className='not-found-text-container'>
                 <header>
-                    <h2>So sorry!</h2>
+                    <h2>
+                        So sorry!
+                    </h2>
                 </header>
-                <p>The page you are looking for cannot be found.</p>
-                <Button type='button' buttonText='Take me home' onClick={() => navigate('/')} />
+                <p>
+                    The page you are looking for cannot be found.
+                </p>
+                <Button
+                    type='button'
+                    buttonText='Take me home'
+                    onClick={() => navigate('/')}
+                />
             </article>
             <div className="not-found-image-container">
-                <img src={notFoundImage} alt='Man that is lost' />
+                <img
+                    src={notFoundImage}
+                    alt='Man that is lost'
+                />
             </div>
         </main>
-    )
+    );
 }
 
 export default PageNotFound;
